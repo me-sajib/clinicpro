64babe4f9ebd50e60ad148641779d40a0af2fd14
me-sajib <me.mrsajib@gmail.com>
2023-06-27 05:02:28 +0600

    feat: add redux context

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,17 @@
 import { useRoutes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import routes from "./routes";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function App() {
   const content = useRoutes(routes)
   return (
-    <AuthProvider>
-      {content}
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        {content}
+      </AuthProvider>
+    </Provider>
   );
 }
 
